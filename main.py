346dda29c31d0d8e63de502c56d5f4c1d4d2858a
DasUser@BEELINK <none@none.none>
2024-04-03 11:36:48 +0700

    add requirements.txt

--- main.py
+++ main.py
@@ -22,36 +22,46 @@ BACKUP_DIRECTORY = "/zPool/zDir/dump"
 BACKUP_ENC_DIRECTORY = "/zPool/zDir/dump/enc"
 BACKUP_FILENAME_LIST = os.listdir(BACKUP_DIRECTORY)
 
+
 def get_current_date_str():
     now = datetime.datetime.now(pytz.timezone(TIME_ZONE))
     return now.strftime('%Y_%m_%d')
+
 
 def get_s3_instance():
     session = boto3.session.Session()
     return session.client(service_name='s3', endpoint_url='https://storage.yandexcloud.net')
 
+
 def upload_dump_to_s3(upl_filename: str, backup_date: str):
     logger.info("Starting upload to Object Storage")
-    #print(f'{S3_DIRNAME}/{get_current_date_str()}/{upl_filename}')
-    get_s3_instance().upload_file(Filename=f'{BACKUP_ENC_DIRECTORY}/{upl_filename}', Bucket=S3_BUCKET_NAME, Key=f'{S3_DIRNAME}/{backup_date}/{upl_filename}')
+    # print(f'{S3_DIRNAME}/{get_current_date_str()}/{upl_filename}')
+    get_s3_instance().upload_file(Filename=f'{BACKUP_ENC_DIRECTORY}/{upl_filename}', Bucket=S3_BUCKET_NAME,
+                                  Key=f'{S3_DIRNAME}/{backup_date}/{upl_filename}')
     logger.info("Uploaded")
+
 
 def get_last_backup_filename():
     s3 = get_s3_instance()
     dumps = s3.list_objects(Bucket=S3_BUCKET_NAME)['Contents']
     print(dumps)
-   # dumps.sort(key=lambda x: x['LastModified'])
-   # last_backup_filename = dumps[-1]
-   # print(f"\U000023F3 Last backup in S3 is {last_backup_filename['Key']}, "
-   # f"{round(last_backup_filename['Size'] / (1024*1024))} MB, "
-   # f"download it")
-   # return last_backup_filename['Key']
-#### TEST #####
 
+
+# dumps.sort(key=lambda x: x['LastModified'])
+# last_backup_filename = dumps[-1]
+# print(f"\U000023F3 Last backup in S3 is {last_backup_filename['Key']}, "
+# f"{round(last_backup_filename['Size'] / (1024*1024))} MB, "
+# f"download it")
+# return last_backup_filename['Key']
+
+
+# TEST
 def upload(file, input):
+    s3 = get_s3_instance()
     transfer = S3Transfer(s3)
     size = float(os.path.getsize(file))
     transfer.upload_file(**input, callback=_progress(file, size, 'Upload'))
+
 
 def _progress(filename, size, ops):
     """ indicator to calculate progress based on filesize"""
@@ -66,11 +76,12 @@ def _progress(filename, size, ops):
             nonlocal _seen_so_far
             _seen_so_far += bytes_amount
             percentage = (_seen_so_far / _size) * 100
-            logging.info("%s: %s  %s / %s  (%.2f%%)" % (_ops, _filename, _seen_so_far, _size,percentage))
+            logger.info("%s: %s  %s / %s  (%.2f%%)" % (_ops, _filename, _seen_so_far, _size, percentage))
+
     return call
 
 
-#### end test ####
+# end test
 
 if __name__ == "__main__":
     try:
@@ -78,20 +89,21 @@ if __name__ == "__main__":
         if arg and arg == "job-end":
 
             logger.info(f"START PROGRAM - {datetime.datetime.now(pytz.timezone(TIME_ZONE))}")
-            curent_date = get_current_date_str()
+            current_date = get_current_date_str()
             for FileName in BACKUP_FILENAME_LIST:
-                if str(curent_date) in FileName and not FileName.endwith(".log"):
+                if str(current_date) in FileName and not FileName.endwith(".log"):
                     logger.info(f"START WORK {FileName} - {datetime.datetime.now(pytz.timezone(TIME_ZONE))}")
-                    #print(f"""{BACKUP_DIRECTORY + FileName}""")
-                    with open((f"""{BACKUP_DIRECTORY}/{FileName}"""), "rb") as file:
-                        operation_status = os.WEXITSTATUS(os.system(f"""gpg --batch --yes -e -a -r FarmBackup@s3.ru -o {BACKUP_ENC_DIRECTORY}/{FileName}.enc   {BACKUP_DIRECTORY}/{FileName}"""))
+                    # print(f"""{BACKUP_DIRECTORY + FileName}""")
+                    with open(f"""{BACKUP_DIRECTORY}/{FileName}""", "rb") as file:
+                        operation_status = os.WEXITSTATUS(os.system(
+                            f"""gpg --batch --yes -e -a -r FarmBackup@s3.ru -o {BACKUP_ENC_DIRECTORY}/{FileName}.enc   {BACKUP_DIRECTORY}/{FileName}"""))
 
                         file.close()
                     logger.info(f"END WORK {FileName} - {datetime.datetime.now(pytz.timezone(TIME_ZONE))}")
 
             for FileName in os.listdir(BACKUP_ENC_DIRECTORY):
                 try:
-                    upload_dump_to_s3(FileName, curent_date)
+                    upload_dump_to_s3(FileName, current_date)
                 except Exception as e:
                     logger.error(e)
                 else:
@@ -101,4 +113,4 @@ if __name__ == "__main__":
             logger.info(f"END PROGRAM - {datetime.datetime.now(pytz.timezone(TIME_ZONE))}")
 
     except IndexError as e:
-        logger.ettor(e)
+        logger.error(e)
